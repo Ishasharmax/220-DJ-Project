8bed354f348dcd65690ca522eb0c80baa3319486
Bria Bradley <bbradley@ithaca.edu>
2018-12-04 11:53:47 -0500

    SongTest

--- SongTest.cpp
+++ SongTest.cpp
@@ -3,36 +3,25 @@
 //
 #include <iostream>
 #include "Song.h"
-#include "MasterSongList.h"
+#include "SongLib.h"
 #include "DJTestLib.h"
 
-void runCarTests(){
-    Car myCar =  Car(1995, "Chevy", "Blazer", 500, 85);
-    std::cout << myCar.toString() << std::endl;
-    printAssertEquals(415, myCar.calcPotentialProfit(), 0.00001);
+void runSongTests(){
+    Song mySong =  Song("Love on Top", "Beyonce", 4.00);
+    std::cout << mySong.toString() << std::endl;
+    printAssertEquals(415, mySong.calcPotentialProfit(), 0.00001);
 
-    myCar.addRepairCost(15);
-    myCar.addRepairCost(100);
-    printAssertEquals(300, myCar.calcPotentialProfit(), 0.00001);
-}
+    mySong.getArtist();
+    mySong.getTitle();
+    printAssertEquals(300, mySong.calcPotentialProfit(), 0.00001);
 
-void runPriorityQueueTest(PriorityQueue *qToTest){
-    std::cout << "---------- testAddAndDequeue ----------"<< std::endl;
-    qToTest->addItem("1. broken bone", 2.5);
-    qToTest->addItem("2. head trauma", 3.4);
-    qToTest->addItem("3. cut on foot", 1.05);
-    qToTest->addItem("4. sore throat", 0.8);
-    qToTest->addItem("5. animal bite", 2.5);
-    qToTest->addItem("6. cut on back", 1.1);
-
-    while (!qToTest->isEmpty()) {
+   /* while (!qToTest->isEmpty()) {
         std::cout << qToTest->dequeueHighestPriorityItem() << std::endl;
-    }
+    }*/
     std::cout << "---- done ----"<< std::endl;;
 }
 
-void runFastDequeueTest(){
-    PriorityQueue* p1 = new LinkedPriorityQueueFastDequeue();
-    runPriorityQueueTest(p1);
-    delete p1;
+int main(){
+    runSongTests();
+    return 0;
 }
